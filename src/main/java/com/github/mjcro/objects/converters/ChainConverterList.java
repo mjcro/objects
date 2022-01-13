2986b3145488a9e6b743f312453e9860ba0a1d91
Gotterdemarung <gotterdemarung@gmail.com>
2022-01-13 18:55:05 +0200

    :sparkles: Chaining simplification

---
+++ src/main/java/com/github/mjcro/objects/converters/ChainConverterList.java
@@ -0,0 +1,53 @@
+package com.github.mjcro.objects.converters;
+
+import com.github.mjcro.objects.ConversionContext;
+import com.github.mjcro.objects.Converter;
+import com.github.mjcro.objects.UnableToConvertException;
+
+import java.util.LinkedList;
+import java.util.Objects;
+import java.util.function.Function;
+
+public class ChainConverterList implements Converter {
+    private final LinkedList<Converter> converters = new LinkedList<>();
+
+    public void addFirst(Converter converter) {
+        converters.addFirst(Objects.requireNonNull(converter, "converter"));
+    }
+
+    public void addFirst(Function<Converter, Converter> factory) {
+        addFirst(factory.apply(ConverterStubbing.INSTANCE));
+    }
+
+    public void addLast(Converter converter) {
+        converters.addLast(Objects.requireNonNull(converter, "converter"));
+    }
+
+    public void addLast(Function<Converter, Converter> factory) {
+        addLast(factory.apply(ConverterStubbing.INSTANCE));
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public <T> T convert(ConversionContext<T> context) throws UnableToConvertException {
+        for (Converter converter : converters) {
+            Object result = converter.convert(context);
+            if (result != ConverterStubbing.STUB) {
+                return (T) result;
+            }
+        }
+
+        throw new UnableToConvertException(context);
+    }
+
+    public static void main(String[] args) {
+        ChainConverterList list = new ChainConverterList();
+        list.addLast(ConverterSame::new);
+        list.addLast(ConverterToString::new);
+        list.addLast(ConverterUnboxing::new);
+        list.addLast(ConverterStringNumbers::new);
+
+        System.out.println(list.convert("5", String.class));
+        System.out.println(list.convert("5", Long.class));
+    }
+}
---
+++ src/main/java/com/github/mjcro/objects/converters/ConverterStubbing.java
@@ -0,0 +1,22 @@
+package com.github.mjcro.objects.converters;
+
+import com.github.mjcro.objects.ConversionContext;
+import com.github.mjcro.objects.Converter;
+
+/**
+ * Special converter instance to be use in chain flatteners.
+ */
+public class ConverterStubbing implements Converter {
+    /**
+     * Stub object that is always returned by this converter
+     */
+    public static final Object STUB = new Object();
+
+    public static final ConverterStubbing INSTANCE = new ConverterStubbing();
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public <T> T convert(ConversionContext<T> context) {
+        return (T) STUB;
+    }
+}
