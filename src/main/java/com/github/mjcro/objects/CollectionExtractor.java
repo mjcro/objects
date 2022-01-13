4f7d8b55046658b8cae90b9c86d4e48f093d3331
Gotterdemarung <gotterdemarung@gmail.com>
2022-01-13 20:36:05 +0200

    :sparkles: Collection extractor

--- src/main/java/com/github/mjcro/objects/CollectionExtractor.java
+++ src/main/java/com/github/mjcro/objects/CollectionExtractor.java
@@ -17,24 +17,113 @@ import java.util.stream.Collectors;
 public class CollectionExtractor {
     private final Converter converter;
 
+    /**
+     * Constructs new collection extractor with given converter.
+     *
+     * @param converter Converter to use, mandatory.
+     */
     public CollectionExtractor(Converter converter) {
         this.converter = Objects.requireNonNull(converter, "converter");
     }
 
+    /**
+     * Converts object from conversion context into list.
+     *
+     * @param source    Source object.
+     * @param target    Target collection's item class.
+     * @param separator Value separator, comma is used by default.
+     * @param <T>       Collection type type.
+     * @return Collection.
+     * @throws UnableToConvertException On conversion error.
+     */
+    public <T> List<T> toList(
+            Object source,
+            Class<T> target,
+            CharSequence separator
+    ) throws UnableToConvertException {
+        return this.toList(new ConversionContext<>(source, target), separator);
+    }
+
+    /**
+     * Converts object from conversion context into list.
+     *
+     * @param context   Conversion context.
+     * @param separator Value separator, comma is used by default.
+     * @param <T>       Collection type type.
+     * @return Collection.
+     * @throws UnableToConvertException On conversion error.
+     */
     public <T> List<T> toList(
             ConversionContext<T> context,
             CharSequence separator
-    ) {
-        return (List<T>) toCollection(context, separator, ArrayList::new);
+    ) throws UnableToConvertException {
+        return (List<T>) this.toCollection(context, separator, ArrayList::new);
     }
 
+    /**
+     * Converts object from conversion context into set.
+     *
+     * @param source    Source object.
+     * @param target    Target collection's item class.
+     * @param separator Value separator, comma is used by default.
+     * @param <T>       Collection type type.
+     * @return Collection.
+     * @throws UnableToConvertException On conversion error.
+     */
+    public <T> Set<T> toSet(
+            Object source,
+            Class<T> target,
+            CharSequence separator
+    ) throws UnableToConvertException {
+        return this.toSet(new ConversionContext<>(source, target), separator);
+    }
+
+    /**
+     * Converts object from conversion context into set.
+     *
+     * @param context   Conversion context.
+     * @param separator Value separator, comma is used by default.
+     * @param <T>       Collection type type.
+     * @return Collection.
+     * @throws UnableToConvertException On conversion error.
+     */
     public <T> Set<T> toSet(
             ConversionContext<T> context,
             CharSequence separator
-    ) {
-        return (Set<T>) toCollection(context, separator, LinkedHashSet::new);
+    ) throws UnableToConvertException {
+        return (Set<T>) this.toCollection(context, separator, LinkedHashSet::new);
     }
 
+    /**
+     * Converts given object to collection.
+     *
+     * @param source    Source object.
+     * @param target    Target collection's item class.
+     * @param separator Value separator, comma is used by default.
+     * @param factory   Collection factory, reference to it's constructor in most cases.
+     * @param <T>       Collection item type.
+     * @return Collection.
+     * @throws UnableToConvertException On conversion error.
+     */
+    public <T> Collection<T> toCollection(
+            Object source,
+            Class<T> target,
+            CharSequence separator,
+            Supplier<Collection<T>> factory
+    ) throws UnableToConvertException {
+        return this.toCollection(new ConversionContext<>(source, target), separator, factory);
+    }
+
+    /**
+     * Converts object from conversion context into collection.
+     *
+     * @param context   Conversion context.
+     * @param separator Value separator, comma is used by default.
+     * @param factory   Collection factory, reference to it's constructor in most cases.
+     * @param <T>       Collection type type.
+     * @return Collection.
+     * @throws UnableToConvertException On conversion error.
+     */
     public <T> Collection<T> toCollection(
             ConversionContext<T> context,
             CharSequence separator,
@@ -45,6 +134,9 @@ public class CollectionExtractor {
         }
         if (factory == null) {
             return toList(context, separator);
+        }
+        if (separator == null) {
+            separator = ",";
         }
 
         List<?> sourceList;
---
+++ src/test/java/com/github/mjcro/objects/CollectionExtractorTest.java
@@ -0,0 +1,39 @@
+package com.github.mjcro.objects;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+
+public class CollectionExtractorTest {
+    @Test
+    public void testToCollectionRaw() {
+        Collection<Long> longs = new CollectionExtractor(Converter.standard()).toCollection(
+                Arrays.asList(
+                        "5",
+                        6,
+                        7L,
+                        Long.valueOf(8)
+                ),
+                Long.class,
+                null,
+                ArrayList::new
+        );
+
+        Assert.assertEquals(longs, Arrays.asList(5L, 6L, 7L, 8L));
+    }
+
+    @Test
+    public void testToCollectionString() {
+        Collection<Long> longs = new CollectionExtractor(Converter.standard()).toCollection(
+                "-3,80, 16",
+                Long.class,
+                null,
+                ArrayList::new
+        );
+
+        Assert.assertEquals(longs, Arrays.asList(-3L, 80L, 16L));
+    }
+}
