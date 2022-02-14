49124bc42b4077ebc5a10a5ff27f3cf3a29c20a5
gotterdemarung <gotterdemarung@gmail.com>
2022-02-14 21:09:27 +0200

    :white_check_mark: Collection extractor test

--- src/test/java/com/github/mjcro/objects/CollectionExtractorTest.java
+++ src/test/java/com/github/mjcro/objects/CollectionExtractorTest.java
@@ -1,11 +1,10 @@
 package com.github.mjcro.objects;
 
+import com.github.mjcro.objects.converters.ConverterSame;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
+import java.util.*;
 
 public class CollectionExtractorTest {
     @Test
@@ -35,5 +34,42 @@ public class CollectionExtractorTest {
         );
 
         Assert.assertEquals(longs, Arrays.asList(-3L, 80L, 16L));
+    }
+
+    @Test
+    public void testMixedDelegation() {
+        Collection<Long> longs = new CollectionExtractor(new ConverterSame()).toCollection(
+                Mixed.wrap(General.CONVERTER, "333,21"),
+                long.class,
+                ",",
+                ArrayList::new
+        );
+
+        Assert.assertEquals(longs.size(), 2);
+        Assert.assertTrue(longs.contains(333L));
+        Assert.assertTrue(longs.contains(21L));
+    }
+
+    @Test(expectedExceptions = ConversionException.class)
+    public void testToCollectionNullContext() {
+        new CollectionExtractor(General.CONVERTER).toCollection(null, ",", ArrayList::new);
+    }
+
+    @Test
+    public void testToSet() {
+        Set<Integer> set = new CollectionExtractor(General.CONVERTER).toSet("1,2,3", int.class, ",");
+        Assert.assertEquals(set.size(), 3);
+        Assert.assertTrue(set.contains(1));
+        Assert.assertTrue(set.contains(2));
+        Assert.assertTrue(set.contains(3));
+    }
+
+    @Test
+    public void testToList() {
+        List<Integer> list = new CollectionExtractor(General.CONVERTER).toList("-8,3,12", int.class, ",");
+        Assert.assertEquals(list.size(), 3);
+        Assert.assertEquals(list.get(0), -8);
+        Assert.assertEquals(list.get(1), 3);
+        Assert.assertEquals(list.get(2), 12);
     }
 }
