629c7027ec517658c5ccf607f31491b2ab0c54ba
Gotterdemarung <gotterdemarung@gmail.com>
2022-03-14 09:56:34 +0200

    :sparkles: Params

---
+++ src/main/java/io/github/mjcro/objects/Param.java
@@ -0,0 +1,75 @@
+package io.github.mjcro.objects;
+
+import java.util.AbstractMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Function;
+
+/**
+ * Represents generic parameter with string name.
+ *
+ * @param <T> Parameter type.
+ */
+public interface Param<T> {
+    /**
+     * Creates new parameter with given name and value.
+     *
+     * @param name  Parameter name.
+     * @param value Parameter value.
+     * @param <T>   Parameter value type.
+     * @return Created parameter.
+     */
+    static <T> Param<T> create(String name, T value) {
+        return null; // TODO
+    }
+
+    /**
+     * @return Param name.
+     */
+    String getName();
+
+    /**
+     * @return Param value.
+     */
+    Optional<T> getValue();
+
+    /**
+     * @return String representation of value.
+     */
+    default Optional<String> getValueString() {
+        return getValue().map(Object::toString);
+    }
+
+    /**
+     * @return {@link Mixed} representation of value.
+     */
+    default Mixed getValueMixed() {
+        return getValueMixed(null);
+    }
+
+    /**
+     * @param converter Converter to use, optional.
+     * @return {@link Mixed} representation of value.
+     */
+    default Mixed getValueMixed(Converter converter) {
+        return Mixed.wrap(converter, getValue().orElse(null));
+    }
+
+    /**
+     * @return Param as map entry.
+     */
+    default Map.Entry<String, T> toMapEntry() {
+        return new AbstractMap.SimpleEntry<>(getName(), getValue().orElse(null));
+    }
+
+    /**
+     * Maps parameter value into other type.
+     *
+     * @param mapper Mapper to user.
+     * @param <R>    Resulting type.
+     * @return New parameter.
+     */
+    default <R> Param<R> map(Function<? super T, R> mapper) {
+        return create(getName(), getValue().map(mapper).orElse(null));
+    }
+}
